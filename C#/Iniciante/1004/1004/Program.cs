5cd0ebca40fc9d93927a72a559788e46925532a0
Joel-Nogueira <joelnogueira733@gmail.com>
2019-11-15 15:06:50 -0300

    Problemas 1004 e 1005

---
+++ C#/Iniciante/1004/1004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("PROD = " + (x * y));
+        }
+    }
+}
---
+++ C#/Iniciante/1005/1005/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y;
+            double media;
+
+            x = float.Parse(Console.ReadLine());
+            y = float.Parse(Console.ReadLine());
+
+            media = ((x * 3.5) + (y * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5"));
+        }
+    }
+}
