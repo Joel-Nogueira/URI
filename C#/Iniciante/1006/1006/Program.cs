055f97d43488addfec2f4d057f4387d1bccda017
Joel-Nogueira <joelnogueira733@gmail.com>
2019-11-15 22:25:11 -0300

    Problema 1006

---
+++ C#/Iniciante/1006/1006/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine()),
+                   b = double.Parse(Console.ReadLine()),
+                   c = double.Parse(Console.ReadLine());
+            double media = ((a * 2) + (b * 3) + (c * 5)) / 10;
+            Console.WriteLine("MEDIA = " + media.ToString("F1"));
+        }
+    }
+}
